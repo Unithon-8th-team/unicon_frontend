abb417dcf93d8d76bde69a5d19267e2e367e84df
seongjinYU <128691265+seongjinYU@users.noreply.github.com>
2025-08-10 17:21:45 +0900

    홈화면, 앱바 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'login/view/login_screen.dart';
 import 'home/view/home_screen.dart';
-import 'chat/view/chat_screen.dart';
-import 'chat/view/chat_initial_screen.dart';
-import 'shop/view/shop_screen.dart';
 
 void main() {
   // 카카오 SDK 초기화
@@ -25,36 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      initialRoute: '/',
-      onGenerateRoute: (settings) {
-        Widget page;
-        switch (settings.name) {
-          case '/':
-            page = const LoginScreen();
-            break;
-          case '/home':
-            page = const HomeScreen();
-            break;
-          case '/chat':
-            page = const ChatInitialScreen();
-            break;
-          case '/chat/room':
-            page = const ChatScreen();
-            break;
-          case '/shop':
-            page = const ShopScreen();
-            break;
-          default:
-            page = const LoginScreen();
-        }
-        
-        // 애니메이션 없는 페이지 전환
-        return PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => page,
-          transitionDuration: Duration.zero,
-          reverseTransitionDuration: Duration.zero,
-        );
-      },
+      home: const LoginScreen(),
     );
   }
 }
